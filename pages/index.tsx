f64e595420606cd931e9cc63996ef326ebc3156a
Y0SH <yosa.agung13@gmail.com>
2021-09-11 15:04:46 +0700

    check data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,40 @@
 import type { NextPage } from "next";
 import { Box, Stack } from "@chakra-ui/react";
 import {
-   Banner,
-   Caraousel,
-   Filter,
-   JoinCommunity,
-   Navbar,
-   ProductsRender,
-   Sidebar,
-   Footer,
+  Banner,
+  Caraousel,
+  Filter,
+  JoinCommunity,
+  Navbar,
+  ProductsRender,
+  Sidebar,
+  Footer,
 } from "../components";
-
+import { UseProductsQuery } from "../query/query";
 const Home: NextPage = () => {
-   return (
-      <Box bg="gray.100" w="100%">
-         <Navbar />
-         <Filter />
-         <Stack direction="row" px="5">
-            <Sidebar />
-            <Box flex={6}>
-               <Banner />
-               <ProductsRender />
-               <Box w="100rem" mt="5">
-                  <Caraousel />
-               </Box>
-               <ProductsRender />
-            </Box>
-         </Stack>
-         <JoinCommunity />
-         <Footer />
-      </Box>
-   );
+  //@ts-ignore
+  const { data, isLoading } = ProductsQuery();
+  console.log(data);
+
+  return (
+    <Box bg="gray.100" w="100%">
+      <Navbar />
+      <Filter />
+      <Stack direction="row" px="5">
+        <Sidebar />
+        <Box flex={6}>
+          <Banner />
+          <ProductsRender />
+          <Box w="100rem" mt="5">
+            <Caraousel />
+          </Box>
+          <ProductsRender />
+        </Box>
+      </Stack>
+      <JoinCommunity />
+      <Footer />
+    </Box>
+  );
 };
 
 export default Home;
--- query/query.ts
+++ query/query.ts
@@ -1,6 +1,13 @@
+import { useQuery } from "react-query";
 import axios from "axios";
 
 const httpClient = axios.create({
   baseURL: "https://shoesmart-test.vercel.app/api",
   withCredentials: true,
 });
+export const UseProductsQuery = () => {
+  useQuery("products", async () => {
+    const axiosResponse = await httpClient.get("/products");
+    return axiosResponse.data;
+  });
+};
