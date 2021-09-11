1958393145caf90ecb6e73f4aa45b2adc55b2a06
mananjabrik <manan.jabrik@gmail.com>
2021-09-12 02:15:11 +0700

    remove react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,12 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { QueryClient, QueryClientProvider } from "react-query";
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </QueryClientProvider>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
 export default MyApp;
